a588b602d82b5a16193aadcdb72551bdd453f3ce
Osman Currim <osman.currim@gmail.com>
2024-04-02 00:46:20 -0400

    fix: paths

--- src/base/.devcontainer/local-features/antidote/install.sh
+++ src/base/.devcontainer/local-features/antidote/install.sh
@@ -7,7 +7,7 @@ set -e
 export PATH=${PATH}
 export ZDOTDIR=${ZDOTDIR}
 
-ln -s "${ZDOTDIR}/.zshrc" "${HOME}/.zshrc"
+ln -s "${ZDOTDIR}/.zshrc" "/home/vscode/.zshrc"
 
 git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR}/antidote
 source "${ZDOTDIR}/antidote/antidote.zsh"
