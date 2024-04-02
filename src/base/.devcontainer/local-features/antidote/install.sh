a83ebdfa05e370d34a9489d5fe745b6455d1e10d
Osman Currim <osman.currim@gmail.com>
2024-04-01 23:56:10 -0400

    fix: paths

--- src/base/.devcontainer/local-features/antidote/install.sh
+++ src/base/.devcontainer/local-features/antidote/install.sh
@@ -5,7 +5,10 @@ su - vscode <<EOF
 set -e
 
 PATH=${PATH}
-source "$(brew --prefix)/share/antidote/antidote.zsh"
+ZDOTDIR=${ZDOTDIR}
+
+git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR}/antidote
+source "${ZDOTDIR}/antidote/antidote.zsh"
 antidote bundle <"${ZDOTDIR}/plugins.txt" >"${ZDOTDIR}/plugins.zsh"
 rm "${ZDOTDIR}/plugins.txt"
 EOF
