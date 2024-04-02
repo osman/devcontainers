e22d21846115b537107522262cf832e7cf9837bf
Osman Currim <osman.currim@gmail.com>
2024-04-01 23:42:30 -0400

    fix: paths

--- src/base/.devcontainer/local-features/gcz/install.sh
+++ src/base/.devcontainer/local-features/gcz/install.sh
@@ -5,10 +5,10 @@ su - vscode <<EOF
 set -e
 
 PATH=${PATH}
-
 source "${NVM_DIR}/nvm.sh"
 nvm use 18
 
-PNPM_HOME=/home/vscode/.config/pnpm
+PNPM_HOME=${PNPM_HOME}
+mkdir -p "${PNPM_HOME}
 pnpm install -g czg
 EOF
